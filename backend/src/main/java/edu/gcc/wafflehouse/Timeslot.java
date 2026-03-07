59a9be8fecb85653346c346960f9eae6ba39bfa8
Ina Tang <inatang75@outlook.com>
2026-03-06 23:06:59 -0500

    Add getters

--- backend/src/main/java/edu/gcc/wafflehouse/Timeslot.java
+++ backend/src/main/java/edu/gcc/wafflehouse/Timeslot.java
@@ -7,9 +7,21 @@ public class Timeslot {
     private LocalTime startTime;
     private LocalTime endTime;
 
-    public Timeslot (char day, LocalTime startTime, LocalTime endTime) {
+    public Timeslot(char day, LocalTime startTime, LocalTime endTime) {
         this.day = day;
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+
+    public char getDay() {
+        return day;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
     }
 }
