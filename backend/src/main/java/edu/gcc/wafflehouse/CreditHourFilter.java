8d4a7948cbf9a5cc6588d964df234208638deedf
Ina Tang <inatang75@outlook.com>
2026-03-06 22:48:09 -0500

    Implement CreditHourFilter (based on Filter modified in filter/course-name branch)

--- backend/src/main/java/edu/gcc/wafflehouse/CreditHourFilter.java
+++ backend/src/main/java/edu/gcc/wafflehouse/CreditHourFilter.java
@@ -1,4 +1,23 @@
 package edu.gcc.wafflehouse;
 
-public class CreditHourFilter {
+import java.util.ArrayList;
+
+/**
+ * @author Ina Tang
+ */
+public class CreditHourFilter extends Filter<Integer> {
+
+    /**
+     * Returns the courses with the given number of credit hours
+     */
+    @Override
+    public ArrayList<Course> apply(ArrayList<Course> courses, Integer pattern) {
+        ArrayList<Course> matchingCourses = new ArrayList<>();
+        for (Course course : courses) {
+            if (course.getCreditHours() == pattern) {
+                matchingCourses.add(course);
+            }
+        }
+        return matchingCourses;
+    }
 }
