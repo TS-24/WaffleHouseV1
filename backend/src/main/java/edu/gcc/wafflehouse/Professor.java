f33e70e75d3163a1caf146430d0381b4be33b5a5
Ina Tang <inatang75@outlook.com>
2026-03-14 01:23:51 -0400

    Make Professor JSON-serializable

--- backend/src/main/java/edu/gcc/wafflehouse/Professor.java
+++ backend/src/main/java/edu/gcc/wafflehouse/Professor.java
@@ -2,7 +2,15 @@ package edu.gcc.wafflehouse;
 
 public class Professor extends Profile {
 
-    public Professor(String s) {
+    public Professor(String name) {
         super();
+        // Parse "Dr. Smith" style names into first/last
+        String[] parts = name.trim().split("\\s+", 2);
+        if (parts.length == 2) {
+            setFirstName(parts[0]);
+            setLastName(parts[1]);
+        } else {
+            setFirstName(name);
+        }
     }
 }
